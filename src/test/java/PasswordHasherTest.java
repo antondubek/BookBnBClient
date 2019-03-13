b26c4b033e6ccc77e6c4bbc4988cbbd2e2292af9
EvaristoRizzo <evaristorizzo11@gmail.com>
2019-03-13 06:22:11 +0000

    Updated PasswordHasherTest

--- src/test/java/PasswordHasherTest.java
+++ src/test/java/PasswordHasherTest.java
@@ -11,19 +11,17 @@ public class PasswordHasherTest {
 
     private String password = "testingthepasswordhasher123";
 
-    PasswordHasher hasher = new PasswordHasher();
-
     @Test
     public void hashPassword() {
-        String hashedPassword = hasher.hashPassword(password);
-        assertEquals(this.hasher.hashPassword(password), hashedPassword);
+        String hashedPassword = PasswordHasher.hashPassword(password);
+        assertEquals(PasswordHasher.hashPassword(password), hashedPassword);
         assertNotEquals(password, hashedPassword);
     }
 
     @Test
     public void samePassword() {
-        String hashedPassword1 = hasher.hashPassword(password);
-        String hashedPassword2 = hasher.hashPassword(password);
+        String hashedPassword1 = PasswordHasher.hashPassword(password);
+        String hashedPassword2 = PasswordHasher.hashPassword(password);
 
         assertEquals(hashedPassword1, hashedPassword2);
     }
