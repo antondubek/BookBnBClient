06889e912075aed7f25a72eb1b5c74fba7c3cba9
EvaristoRizzo <evaristorizzo11@gmail.com>
2019-04-02 01:31:07 +0100

    changed server to heroku

--- src/main/java/client/Controller.java
+++ src/main/java/client/Controller.java
@@ -26,8 +26,8 @@ import java.util.ArrayList;
  */
 public class Controller {
 
-    //private static String address = "http://antondubek-bookbnb.herokuapp.com";
-    private static String address = "http://localhost:8080";
+    private static String address = "http://antondubek-bookbnb.herokuapp.com";
+    //private static String address = "http://localhost:8080";
 
     public static String name;
     public static String email;
